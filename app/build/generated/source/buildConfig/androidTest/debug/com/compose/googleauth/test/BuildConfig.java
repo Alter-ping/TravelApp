83ad39b962bd436ad9514f4cd3344fbb86cf2788
Alter-ping <lukas.aleksa@gmail.com>
2022-11-12 00:41:21 +0200

    initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/compose/googleauth/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.compose.googleauth.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.compose.googleauth.test";
+  public static final String BUILD_TYPE = "debug";
+}
